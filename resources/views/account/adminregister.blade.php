d01c787a2f147256e41c70bd3c909c99352188d1
Hisui3003 <yatogod22@gmail.com>
2024-03-11 23:59:46 +0800

    adminregister, login, tenantregister

--- resources/views/account/adminregister.blade.php
+++ resources/views/account/adminregister.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-@include('layout.header')
+{{-- @include('layout.header')
 
-@include('layout.nav')
+@include('layout.nav') --}}
 
 <form action="{{route('adminregister.post')}}" method="post">
     @if($errors->any())
@@ -19,9 +19,9 @@
 @csrf
 
 {{-- register --}}
-<div class="container pt-16 pb-28 bg-cover bg-center bg-no-repeat" style="background-image: url('https://images.pexels.com/photos/8292894/pexels-photo-8292894.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')">
-    <div class="max-w-lg mx-auto bg-white shadow-md px-6 py-7 rounded-md overflow-hidden">
-        <h2 class="text-2xl uppercase font-medium mb-1">Signup</h2>
+<div class="bg-gradient-to-r from-red-600 via-red-500 to-red-800 min-h-screen items-center justify-center py-5">
+    <div class="max-w-2xl mx-auto bg-white shadow-md px-6 py-7 rounded-md overflow-hidden">
+        <h2 class="text-2xl uppercase font-medium mb-1">Admin Signup</h2>
         <p class="text-gray-600 mb-6 text-sm">
             Register to
             <span class="font-semibold text-lg text-center text-red-500 mb-2">FindFlat</span>
@@ -72,7 +72,7 @@
                 </div>
                 <div class="mt-4">
                     <button type="submit"
-                    class="block w-full py-2 text-center text-white bg-red-600 border border-primary rounded hover:bg-transparent hover:text-primary transition uppercase font-roboto font-medium">
+                    class="block w-full py-2 text-center text-white bg-red-600 border border-red-500 rounded-2xl hover:scale-105  hover:font-bold transition uppercase font-roboto font-medium">
                         Sign up
                     </button>
                 </div>
@@ -84,6 +84,6 @@
         </p>
     </div>
 </div>
-@include('layout.footer');
+{{-- @include('layout.footer'); --}}
 @endsection
 
